8e2d13cf929e9237e0396203f9d8af7a7f78fc7e
Zain Nasir <49543216+zainasir@users.noreply.github.com>
2025-04-07 17:09:24 -0400

    import pandas

--- update_waypoints.py
+++ update_waypoints.py
@@ -6,6 +6,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import pandas as pd
 
 # Google Sheets API
 # If modifying these scopes, delete the file token.pickle.
