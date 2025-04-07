beeb7950e84fbf3af78a1d6d9c6d199468db8d20
Zain Nasir <49543216+zainasir@users.noreply.github.com>
2025-04-07 18:54:56 -0400

    geocode approved installations

--- update_waypoints.py
+++ update_waypoints.py
@@ -6,15 +6,17 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import googlemaps
 import pandas as pd
+from enum import Enum
 
 # Google Sheets API
-# If modifying these scopes, delete the file token.pickle.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
 # The ID and range of a spreadsheet.
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
-RANGE = "A:K"
+RANGE = "A:T"
+FILTER_BY_COL_IDX = [0,5,8,9,10,19]
 
 # Geocoding API
 GEOCODING_API_KEY = os.getenv("GEOCODING_API_KEY")
@@ -46,19 +48,34 @@ def spreadsheet_to_pandas(creds):
         print("WARN: No data found.")
         return
 
-    print(type(values))
-    print(type(values[0]))
+    # Convert to pandas dataframe
+    df = pd.DataFrame(values[1:], columns=values[0], dtype=str).fillna("")
 
-    df = pd.DataFrame(values[1:], columns=values[0])
-    print(df.head())
+    return df
 
-    print("Approved:")
-    for row in values:
-        print(f"{row[0]}")
+# Geocode given city, state, and country
+def geocode(city, state, country):
+    gmaps = googlemaps.Client(key=GEOCODING_API_KEY)
+    geocode_result = gmaps.geocode("%s %s %s" % (city, state, country))
+    if not geocode_result:
+        geocode_result = gmaps.geocode("%s %s" % (state, country))
+    if not geocode_result:
+        geocode_result = gmaps.geocode(country)
+    if not geocode_result:
+        return None
+    
+    return geocode_result
 
 def main():
     creds = service_account.Credentials.from_service_account_file(GOOGLE_SERVICE_ACCOUNT, scopes=SCOPES)
-    spreadsheet_to_pandas(creds)
+    df = spreadsheet_to_pandas(creds).fillna("")
+    df_approved = df[df[df.columns[0]].str.strip().str.lower() == "yes"]
+    
+    print("Geocoding waypoints...")
+    geocode_waypoints = []
+    for index, row in df_yes.iterrows():
+        curr_waypoint = [row.iloc[col] for col in FILTER_BY_COL_IDX]
+        print(geocode(curr_waypoint[8], curr_waypoint[9], curr_waypoint[10]))
 
 if __name__ == "__main__":
   main()
