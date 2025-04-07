8c38e9bc6946c614e413638eb4e1932ab11dadac
Zain Nasir <49543216+zainasir@users.noreply.github.com>
2025-04-07 16:34:16 -0400

    use google service account

--- update_waypoints.py
+++ update_waypoints.py
@@ -1,6 +1,7 @@
 import os.path
 
 from google.auth.transport.requests import Request
+from google.oauth2 import service_account
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -17,6 +18,9 @@ COLS = "A:A F:M"
 # Geocoding API
 GEOCODING_API_KEY = os.getenv("GEOCODING_API_KEY")
 
+# Google Service Account
+GOOGLE_SERVICE_ACCOUNT = os.getenv("GOOGLE_SERVICE_ACCOUNT")
+
 # Specified directory and file
 SRC_DIR = "./src"
 WAYPOINTS = "%s/waypoints.json" % SRC_DIR
@@ -26,24 +30,7 @@ def main():
   """Shows basic usage of the Sheets API.
   Prints values from a sample spreadsheet.
   """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+  creds = service_account.Credentials.from_service_account_info(GOOGLE_SERVICE_ACCOUNT, scopes=SCOPES)
 
   try:
     service = build("sheets", "v4", credentials=creds)
